a879ac8ea09411c6d9db7216190a6a3fc4e2764d
valentinpate <valentinp261@gmail.com>
2023-11-23 22:28:38 -0300

    fixed master.

--- client/pelis-mern/src/Hero.jsx
+++ client/pelis-mern/src/Hero.jsx
@@ -1,22 +1,66 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './sketch.css'
-import NavBar from './NavBar';
 import Header from './Header';
+
+let key = true;
 
 function Hero({newArray}){
     let [num, setNum]=useState(1)
+    let array = [0,1,2,3,4,5]
+    let [hover, setHover] = useState(true)
 
-    const changeMovie=(event)=>
+    const changeMovie=async (event)=>
         {
-            const number =Number(event.target.textContent) 
+        key = false
+        const number =Number(event.target.textContent) 
         setNum(number)
-        console.log(num)
-       }
+    };
+
+    const slide = async()=>{
+        if(num == array[num]){
+            await new Promise(resolve => setTimeout(resolve, 10000));
+
+            if(key){
+                setNum(num+1)
+            }
+        
+        }
+    }; slide()
+        
+    const changeMovieMinus = ()=>{
+        key = false
+        if(num != 1){
+            setNum(num-1)
+            console.log(num)
+        }  
+    };
+
+    const changeMoviePlus = ()=>{
+        key = false
+        if (num != 6){
+            setNum(num+1)
+            console.log(num)
+        }  
+    };
+
+    const handleHover = ()=>{
+        setHover(false)
+    };
+
+    const handleUnhover = ()=>{
+       setHover(true)
+    };
 
     const background = {
-        backgroundImage: newArray.length>0?`linear-gradient(to bottom, transparent 0%, #000000 97%), url(${newArray[num-1].primaryImage.url})`:null,
+        backgroundImage: newArray.length>0?`linear-gradient(to bottom, transparent 0%, #000000 95%),url(${newArray[num-1].primaryImage.url}) `:null,
         backgroundSize:'cover',
         backgroundPosition:'center'
+    };
+
+    const hoverAuto ={//hover para slide
+        fontSize:"2rem",
+        color:"var(--red)",
+        marginTop:"0.2em"
     };
     
   return (
@@ -44,14 +88,23 @@ function Hero({newArray}){
                             <a href="#instagram"><i class="bi bi-instagram"></i></a>
                         </div> 
                         <div class="hero-slide d-flex position-absolute">
-                            <button><i class="bi bi-arrow-left"></i></button>
+                            <button onClick={changeMovieMinus}><i class="bi bi-arrow-left"></i></button>
                             <div class="hero-slide-page d-flex">
+                                
                             {
                                 
                                 newArray.map((item,index)=>
                                 <>
+                                    {index<=5?
+                                     <p onMouseEnter={handleHover}
+                                        onMouseLeave={handleUnhover}
+                                        style={num == index+1 && hover ?hoverAuto:null} 
+                                        id={index} 
+                                        onClick={changeMovie} 
+                                        key={index}><a class="page-link" href="#" >{index<=5?"0"+(index+1):null} </a></p>:null
 
-                                    <p id={index} onClick={changeMovie}><a class="page-link" href="#" >{index<=8?"0"+(index+1):index+1} </a></p>
+                                    }
+                                   
 
                                 </>
                                 )
@@ -59,7 +112,7 @@ function Hero({newArray}){
                             
                             }
                             </div>
-                            <button><i class="bi bi-arrow-right"></i></button>
+                            <button onClick={changeMoviePlus} ><i class="bi bi-arrow-right"></i></button>
                         </div>
                     </div>
                 </section>
