55d5f62d0c7389fda428d62cbd316be8d74251ba
valentinpate <valentinp261@gmail.com>
2023-11-28 16:57:45 -0300

    estrenos parte 1

--- client/pelis-mern/src/Home.jsx
+++ client/pelis-mern/src/Home.jsx
@@ -2,11 +2,13 @@ import './sketch.css'
 import Hero from './Hero';
 import News from './News';
 import Footer from './Footer'
+import Estrenos from './Estrenos';
 function Home({newArray,setIdTrailer,trailer,setTrailer}){
   
   return (
     <>
         <Hero newArray={newArray} trailer={trailer} setTrailer={setTrailer} setIdTrailer={setIdTrailer}></Hero>
+        <Estrenos></Estrenos>
         <News></News>
         <Footer></Footer>
     </>
