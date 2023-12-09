46b9e70b51fa673aede5dc3413969921c1b6b0c9
valentinpate <valentinp261@gmail.com>
2023-12-09 12:02:35 -0300

    log out

--- client/pelis-mern/src/Header.jsx
+++ client/pelis-mern/src/Header.jsx
@@ -1,12 +1,13 @@
+import axios from 'axios';
 import { useContext , useEffect} from 'react';
 import { UserContext } from './UserContext';
 import NavBar from './NavBar';
 import './sketch.css'
-import {NavLink, Link} from "react-router-dom"
+import {NavLink, Link, useNavigate} from "react-router-dom"
 
 function Header(){
   const {user , setUser} = useContext(UserContext)
-
+  const navigate = useNavigate()
    // Al cargar el componente, verificamos si la información del usuario está en localStorage
    useEffect(() => {
     const savedUser = localStorage.getItem('user');
@@ -24,7 +25,13 @@ function Header(){
     }
   }, [user]);
 
-  
+  const LogOut = async () => {
+    localStorage.removeItem('user')
+    const data = await axios.request({method:"GET",url:"http://localhost:3001/logout"})
+    console.log(data)
+    navigate("/")
+  }
+
   return (
     <>
     <header class="d-flex justify-content-between py-4 px-5">
@@ -34,9 +41,9 @@ function Header(){
                 <a href="#search"><button class="btn-search"><i class="bi bi-search px-5"></i></button></a>
                 { user ?(
                   <div className='dropdown'>
-                    <button className='btn px-4 colorButton ms-2 dropdown-toggle' type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{user.name}</button>
+                    <button className='btn px-4 colorButton ms-2 dropdown-toggle' type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{user.name}</button>
                     <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                      <Link className="dropdown-item" to="/logout">Logout</Link>
+                      <a className="dropdown-item" onClick={LogOut}>Logout</a>
                      </div>
                   </div>
                 ) : ( <NavLink to="/signin"><button class="btn px-4 colorButton ms-2">Sign in</button></NavLink>
