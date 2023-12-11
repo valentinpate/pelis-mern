181c497a733d5903b59d6c5d05b063f304e95432
valentinpate <valentinp261@gmail.com>
2023-12-11 11:11:47 -0300

    create profile part 1

--- server/models/User.js
+++ server/models/User.js
@@ -15,6 +15,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    profiles:[{
+        name:{
+            type:String,
+            required:true
+        },
+        myList:[]
+    }]
 })
 
 userSchema.pre("save", async function(next){   //Agregue este para encriptar las contraseñas
