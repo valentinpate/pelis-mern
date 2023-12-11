cd403fbc39378a4c3142dfc1b33aeff3a584dd09
martin bottaro <martinbottaro34@gmail.com>
2023-12-11 17:56:22 -0300

    profile valen

--- client/pelis-mern/src/pages/profile/Profiles.jsx
+++ client/pelis-mern/src/pages/profile/Profiles.jsx
@@ -1,49 +1,19 @@
-import { useState, useContext } from 'react'
-import {NavLink, Navigate} from "react-router-dom"
-import { UserContext } from '../../UserContext';
-import axios from 'axios'
-import Header from '../../components/Header';
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { useEffect } from "react";
 
-const Profiles = () => {
-
-
- 
-
-  return (
-    <>
-    <Header/>
-    <div class="d-flex justify-content-center">
-        <h1> who's watching?</h1>
-    </div>
-     
-    <div class="d-flex justify-content-center">
-    {
+function Profiles(){
+    useEffect(()=>{
+        async function obtainProfiles(){
+            const data = await axios.request({method:"GET",url:"http://localhost:3001/profiles"%7D)
+            console.log(data)
+        }
+        obtainProfiles()
+    },[])
+    return(
         <>
-            <div class='m-5 '>
-            <img width="140" height="140" class="bd-placeholder-img rounded-circle" src="/blank_user.png" alt="" />
-            <h4 class="fw-normal text-center">Martu</h4>
-            </div>
-            <div class='m-5'>
-            <img width="140" height="140" class="bd-placeholder-img rounded-circle" src="/blank_user.png" alt="" />
-            <h4 class="fw-normal text-center">Martu</h4>
-            </div>
-            <div class='m-5'>
-            <img width="140" height="140" class="bd-placeholder-img rounded-circle" src="/blank_user.png" alt="" />
-            <h4 class="fw-normal text-center">Martu</h4>
-            </div>
-            <div class="d-flex flex-column justify-content-center">
-            <Link><i class="rounded-circle fa-solid fa-plus bg-white"></i></Link>
-            <Link><i class= "fa-solid fa-pen-to-square bg-white rounded-circle"></i></Link>
-            </div>
+            <h1 class="text-light">Perfiles</h1>
         </>
+    )
+}
 
-    }
-    </div>
-    
-    </>
-  
-  );
-};
-
-export default Profiles;
+export default Profiles
