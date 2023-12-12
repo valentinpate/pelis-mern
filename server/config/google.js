c207892a89842e339079982056f1d3130bd5670d
NicoLopezBjj <lopeznnr@gmail.com>
2023-12-12 11:57:50 -0300

    add config/google.js

---
+++ server/config/google.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const User = require('../models/User')
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3001/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
