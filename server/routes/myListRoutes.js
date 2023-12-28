8d71d9093f4f143096ff2da76b5c35e52191ce62
NicoLopezBjj <lopeznnr@gmail.com>
2023-12-28 13:21:49 -0300

    add routes and controllers mylist

---
+++ server/routes/myListRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const myListControllers = require('../controllers/myListControllers')
+
+
+router.post("/add-to-my-list",myListControllers.add_to_my_list)
